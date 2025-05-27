f1e5c9a9010c4524845ea5554fe6b57af6bd6735
nicolasfr-dev <nicolasfr.dev@gmail.com>
2025-05-27 20:34:13 -0300

    feat: moved back images to public

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,26 @@
-const HomePage = () => {
-    return (
-    <>
-        <section className="bg-light-gray-3">
-        
-        <p className="text-primary">Olá mundo</p>
+import Section from "../components/Section";
+import Collections from "../components/Collections";
+import { Link } from "react-router-dom";
 
-        </section>
+const HomePage = () => {
+  return (
+    <>
+      <div className="px-22 pt-7 pb-5">
+        <Section
+          title="Coleções em destaque"
+          link={<Link to="/produtos">Ver tudo</Link>}
+          content={<section className="flex max-w-full gap-2">
+              <Collections className="grow" off="30" img="public/collection-1.png" />
+              <Collections className="grow" off="30" img="public/collection-2.png" />
+              <Collections className="grow" off="30" img="public/collection-3.png" />
+
+          </section>}
+        />
+
+        <img src="" alt="" />
+      </div>
     </>
-     );
-}
- 
+  );
+};
+
 export default HomePage;
