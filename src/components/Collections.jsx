c2680b44b524f5d2d105a82d4ef5048fc9e15dde
nicolasfr-dev <nicolasfr.dev@gmail.com>
2025-05-30 19:41:21 -0300

    feat: added a placeholder gallery collection and a a section component

--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -1,12 +1,12 @@
-const Collections = ({off, img}) => {
+const Collections = ({off, img, className = ''}) => {
   return (
     <>
-      <figure className="relative w-full flex grow">
-        <span className="absolute text-sm font-bold left-8 top-5 py-1 px-3 rounded-2xl bg-off text-dark-gray-2">
+      <figure className={`relative flex flex-col justify-between p-2 rounded-lg bg-light-blue-100 ${className}`}>
+        <span className="absolute text-sm font-bold left-13 top-10 py-1 px-3 rounded-2xl bg-off text-dark-gray-2">
           {off}% OFF
         </span>
-        <img className="rounded-lg" src={img} alt="" />
-        <button className="absolute left-8 bottom-15 bg-white w-40 h-10 rounded-lg text-primary cursor-pointer active:bg-primary active:text-white">
+        <img className="rounded-lg w-full object-cover" src={img} alt="" />
+        <button className="absolute left-13 bottom-15 bg-white w-42 h-12 rounded-lg font-bold text-primary cursor-pointer hover:bg-secondary hover:text-white">
           Comprar
         </button>
       </figure>
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,12 @@
+const Gallery = () => {
+    return ( 
+        <> 
+            <figure className="w-full">
+                <img className="w-full" src="public/home-slide-6.jpeg" alt="" />
+
+            </figure>
+        </>
+     );
+}
+ 
+export default Gallery;
