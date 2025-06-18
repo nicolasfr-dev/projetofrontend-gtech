a6647393446814fb824417b2f22afc52527da549
nicolasfr-dev <nicolasfr.dev@gmail.com>
2025-06-17 21:35:05 -0300

    feat: added category hover animations

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,16 +1,23 @@
-const Category = ({image, title}) => {
-  //Componente de categoria da home page
-  
+const Category = ({ image, hover, title }) => {
   return (
-    <> 
-    <div>
-      <div className="justify-items-center content-center rounded-full w-35 h-35 bg-white">
-        <img src={image} className="scale-125"/>
+    <div className="flex flex-col items-center group cursor-pointer">
+      <div className="relative flex justify-center items-center rounded-full w-[140px] h-[140px] bg-white overflow-hidden">
+        <img
+          src={image}
+          alt={title}
+          className="scale-125 transition-opacity duration-300 opacity-100 group-hover:opacity-0 group-hover:scale-[1.35]"
+        />
+        <img
+          src={hover}
+          alt={title}
+          className="scale-125 transition-opacity duration-300 opacity-0 group-hover:opacity-100 absolute group-hover:scale-[1.35]"
+        />
       </div>
 
-        <h4 className="mt-5 text-dark-gray-2 justify-self-center font-bold">{title}</h4>
+      <h4 className="mt-5 text-dark-gray-2 font-bold group-hover:text-primary transition-colors duration-300">
+        {title}
+      </h4>
     </div>
-    </>
   );
 };
 
