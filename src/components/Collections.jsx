a4f4d08d0ecb2e54978737267dc75d4d6c7b5cc3
nicolasfr-dev <nicolasfr.dev@gmail.com>
2025-06-19 15:16:47 -0300

    feat: add functionality to category, collections and footer components

--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -1,11 +1,19 @@
-const Collections = ({off, img, className = ''}) => {
+const Collections = ({ off, img, className = "" }) => {
   return (
     <>
-      <figure className={`relative flex flex-col justify-between p-2 rounded-lg bg-light-blue-100 ${className}`}>
-        <span className="absolute text-sm font-bold top-5 left-5 py-1 px-3 rounded-2xl bg-off text-dark-gray-2">
+      <figure
+        className={`relative flex flex-col justify-between p-2 rounded-lg bg-light-blue-100 ${className}`}
+      >
+        <span className="absolute text-sm font-bold z-10 top-5 left-5 py-1 px-3 rounded-2xl bg-off text-dark-gray-2">
           {off}% OFF
         </span>
-        <img className="rounded-lg w-full object-contain" src={img} alt="" />
+        <div className="rounded-lg w-full object-contain overflow-hidden">
+          <img
+            className="rounded-lg w-full object-contain hover:scale-105"
+            src={img}
+            alt=""
+          />
+        </div>
         <button className="absolute left-5 bottom-15 bg-white w-42 h-12 rounded-lg font-bold text-primary cursor-pointer hover:bg-secondary hover:text-white">
           Comprar
         </button>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import Logo from "../assets/logo-footer.svg";
 import Instagram from "../assets/instagram.svg";
 import Facebook from "../assets/facebook.svg";
 import Twitter from "../assets/twitter.svg";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -9,9 +10,10 @@ const Footer = () => {
       <footer className="bg-dark-gray text-white pt-15 px-75 pb-10 w-full ">
         <section className="gap-10 flex border-b-2 border-dark-gray-2">
           <div className="w-full">
-              <img className="pb-5 mr-10" src={Logo} alt="" />
+            <img className="pb-5 mr-10" src={Logo} alt="" />
             <p className="text-white text-base font-light">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore.
             </p>
 
             <div className="inline-flex gap-3 mt-10">
@@ -34,25 +36,32 @@ const Footer = () => {
           </div>
 
           <div className="w-3/4">
-              <h5 className="font-semibold">Categorias</h5>
-            <ul className="my-8 font-light space-y-5">
-              <li>Camisetas</li>
-              <li>Calças</li>
-              <li>Bonés</li>
-              <li>Headphones</li>
-              <li>Tênis</li>
-            </ul>
+            <h5 className="font-semibold">Categorias</h5>
+            <Link to={"/produtos"}>
+              <ul className="my-8 font-light space-y-5">
+                <li>Camisetas</li>
+                <li>Calças</li>
+                <li>Bonés</li>
+                <li>Headphones</li>
+                <li>Tênis</li>
+              </ul>
+            </Link>
           </div>
 
           <div className="w-3/4">
-              <h5 className="font-semibold">Contato</h5>
+            <h5 className="font-semibold">Contato</h5>
             <ul className="my-8 font-light space-y-5">
-              <li>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</li>
+              <li>
+                Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE,
+                60150-161
+              </li>
               <li>(85) 3051-3411</li>
             </ul>
           </div>
         </section>
-        <span className="mt-10 text-xs block text-center font-light">@ 2022 Digital College</span>
+        <span className="mt-10 text-xs block text-center font-light">
+          @ 2022 Digital College
+        </span>
       </footer>
     </>
   );
